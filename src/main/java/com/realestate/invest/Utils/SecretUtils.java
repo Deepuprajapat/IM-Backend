0b0bcb4418b96acf96a9f7439394ac398b841ecb
Deepak kumar <153892296+Deepuprajapat@users.noreply.github.com>
2025-05-19 17:27:51 +0530

    Create SecretUtils.java

---
+++ src/main/java/com/realestate/invest/Utils/SecretUtils.java
@@ -0,0 +1,15 @@
+package com.realestate.invest.Utils;
+
+public class SecretUtils 
+{
+    /*
+     * AWS Credentials
+    */
+    public static String APIURL = "http://servermsg.com/api/SmsApi/SendSingleApi";
+    public static String USERID = "Invest";
+    public static String PASSWORD = "alva1032AL";
+    public static String SENDERID = "IMANGO";
+    public static String MSG = "%20is%20your%20OTP.%20Please%20enter%20the%20OTP%20to%20verify%20" +
+    "your%20mobile%20number.%20For%20more%20info%20visit%20investmango.com";
+    public static String ENTITYID = "1701158219615298561";
+    public static String TEMPLATEID = "1707165293637284915";
